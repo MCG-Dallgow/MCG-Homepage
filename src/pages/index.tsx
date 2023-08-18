75a3b0111f4a185a9aac66acae38d781c1782c56
SLH335 <114884726+SLH335@users.noreply.github.com>
2023-08-18 12:52:02 +0200

    Add initial project structure
    Generated initial project structure with "Create T3 App", including
    Next.js, TypeScript and Tailwind CSS. (https://create.t3.gg)

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>MCG Dallgow</title>
+        <meta name="description" content="Die Homepage des Marie-Curie-Gymnasiums Dallgow-Döberitz" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>
+          <h1>
+            Willkommen!
+          </h1>
+        </div>
+      </main>
+    </>
+  );
+}
