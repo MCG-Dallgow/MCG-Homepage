d2793a29b99c67697f7bd94ac99dac1110d11561
SLH335 <114884726+SLH335@users.noreply.github.com>
2023-08-18 12:57:52 +0200

    Add content to page "/unsere-schule/schulleben"

---
+++ src/app/unsere-schule/schulleben/page.tsx
@@ -0,0 +1,36 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Schulleben</h1>
+      <section id="patenprojekt">
+        <h2>Patenprojekt</h2>
+        <div>
+          <p>
+            Das Patenprojekt begleitet alljährlich die neuen Siebtklässler durch
+            ihr erstes Jahr am MCG. Mit der Unterstützung durch die freiwillige
+            Arbeit der Patinnen und Paten, welche aus der Oberstufe stammen,
+            soll ihnen die Umstellung und Eingewöhnung an ihrer neuen Schule
+            deutlich einfacher fallen.
+          </p>
+          <p>
+            An ihrem ersten Schultag empfangen wir die neuen Schülerinnen und
+            Schüler vor der Schule und bringen sie zu ihren neuen
+            Klassenlehrern. Am Nachmittag gibt es dann eine Rally durch die
+            Schule, um sich mit der neuen Umgebung besser vertraut zu machen.
+            <br />
+            In der ersten Schulwoche folgt dann unsere traditionelle „Wanderung
+            durch die Döberitzer Heide“. Bei dieser Veranstaltung lernen sich
+            die Schüler im Klassenverband besser kennen und man verbringt einen
+            gemeinsamen Tag im Naturschutzgebiet Döberitzer Heide. Über das
+            restliche Schuljahr hinweg werden weitere Veranstaltung wie
+            Grillabende, Weihnachtsfeste oder ein Winterball organisiert.
+          </p>
+          <p>
+            Wenn es Probleme beim Verständnis des Schulstoffes gibt, stehen die
+            Paten jederzeit bereit und geben gerne kostenlos Nachhilfe.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
