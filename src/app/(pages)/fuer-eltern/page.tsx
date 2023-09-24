58c93787c1b2d9b501e04750781e2235554ad3ae
Jan Luca <90137253+TheMelone2@users.noreply.github.com>
2023-09-24 18:32:53 +0200

    Add: "Schliessfaecher" & "fuer-eltern" => WiP

---
+++ src/app/(pages)/fuer-eltern/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+    return(
+    <div>
+      <h1>Für Eltern</h1>
+      <section>
+        
+      </section>
+    </div>
+    );
+  }
+  
---
+++ src/app/(pages)/fuer-eltern/schliessfaecher/page.tsx
@@ -0,0 +1,36 @@
+export default function Page() {
+    return(
+    <div>
+        <h1>Schließfächer am MCG</h1>
+        <section>
+            <div className="tutorial">
+                <h2>Wie miete ich ein Schließfach?</h2>
+                <p>
+                    Schließfächer können von Schülerinnen und Schülern gemietet werden. Die Miete beträgt 2,60 € pro Monat (31,20 €/ 12 Monate) und mit <a href="https://www.astradirect.de/schueler">Schutzbrief</a> 43,20 € pro 12 Monate.
+                    <br></br>
+                    Die Schließfächer werden vom Unternehmen ,,Astradirect" zur Verfügung gestellt.
+                </p>
+                <p>
+                    <a href="https://www.astradirect.de/schliessfaecher/" target="_blank">Hier</a> können Sie sich über die Schließfächer informieren.
+                </p>
+                <br></br>
+                <h2>Wie miete ich ein Schließfach?</h2>
+                <p id="tutorial-step"> 1. Gehen Sie auf die Website von <a href="https://www.astradirect.de/fach-mieten/schule-waehlen">Astradirect</a></p>
+                <p id="tutorial-step"> 2. Geben Sie bei Ort der Schule "Dallgow-Döberitz" ein und wählen Sie bei dem Namen der Schule das Marie-Curie-Gymnasium</p>
+                <p id="tutorial-step"> 3. Wählen Sie den Standort des Schließfaches (Am Marie Curie Gymnasium können Sie zwischen drei Standorten wählen!)</p>
+                <p id="tutorial-step"> 4. Wählen Sie die gewünschte Schließfachgröße aus</p>
+                <p id="tutorial-step"> 5. Wählen Sie die gewünschte Mietdauer aus</p>
+                <p id="tutorial-step"> 6. Wählen Sie die gewünschte Zahlungsart aus</p>
+                <p id="tutorial-step"> 7. Geben Sie Ihre Daten ein</p>
+                <p id="tutorial-step"> 8. Bestätigen Sie Ihre Bestellung</p>
+                <br></br>
+                <h2>Wie kann ich mein Schließfach nutzen?</h2>
+                <p> Sie können Ihr Schließfach mit einem Zahlencode öffnen. Der Zahlencode wird Ihnen per E-Mail zugeschickt.</p>
+                <p> Die Schließfächer finden Sie im Haupteingang Flur (Treppenhaus C), im Treppenhaus zwischen C und D, sowie im Keller (Raum UG.60).</p>
+                <br></br>
+            </div>
+
+        </section>
+    </div>
+    );
+    }
